b4dec5e8a35b84bd9651eb3f5d8aea4b4ac1dd6c
RafaelFronteira <rafael_fronteira@hotmail.com>
2022-10-18 22:23:33 -0300

    nao eh o primeiro commit...

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+    }
+});
+
+app.use(cors());
+
+let players = [];
+
+
+io.on('connection', (socket) => {
+    socket.on('add', (player) => {
+        socket.emit('newPlayer', { player, players: players.filter(p => p.id !== player.id) });
+        socket.broadcast.emit('somePlayerAdded', player);
+        players.push(player);
+    });
+
+    socket.on('move', (info) => {
+        players.forEach(p => {
+            if (p.id === info.id) {
+                p.position = info.position;
+            }
+        });
+
+        socket.broadcast.emit('playerMove', {id: info.id, position: info.position});
+    });
+
+    socket.on('remove', (player) => {
+        players = players.filter(p => p.id !== player);
+        socket.broadcast.emit('somePlayerRemoved', player);
+    })
+});
+
+server.listen(3000, () => {
+    console.log('server is alive');
+});
