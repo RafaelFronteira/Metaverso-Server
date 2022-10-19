f03475df03d3525207fc18abfc98259453b2aacb
RafaelFronteira <rafael_fronteira@hotmail.com>
2022-10-18 22:51:39 -0300

    change cors origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "*",
+      origin: "https://rafaelfronteira.github.io",
     }
 });
 
